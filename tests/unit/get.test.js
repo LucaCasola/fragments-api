748f701ad0103e323a6a035e9809fa556d826866
Luca Casola <lcasola@myseneca.ca>
2025-05-20 17:00:45 -0400

    Create get.test.js

---
+++ tests/unit/get.test.js
@@ -0,0 +1,27 @@
+// tests/unit/get.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('GET v1 fragments', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', async () => {
+    await request(app).get('/v1/fragments').expect(401)
+  });
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', async () => {
+    await request(app).get('/v1/fragments').auth('invalid@email.com', 'wrongpassword123').expect(401)
+  });
+
+  // Using a valid username/password pair should give a success result with a .fragments array
+  test('authenticated users get a fragments array', async () => {
+    const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe('ok');
+    expect(Array.isArray(res.body.fragments)).toBe(true);
+  });
+
+  // TODO: we'll need to add tests to check the contents of the fragments array later
+});
