ba5f034e79f67fccea9ae239d091dc40169a7fd7
Luca Casola <lcasola@myseneca.ca>
2025-06-05 20:21:54 -0400

    Implement fragment db calls & unit tests

---
+++ src/model/data/memory/index.js
@@ -0,0 +1,104 @@
+const MemoryDB = require('./memory-db');
+
+// Create two in-memory databases: one for fragment metadata and the other for raw data
+const data = new MemoryDB();
+const metadata = new MemoryDB();
+
+
+/**
+ * Write a fragment's metadata to memory db
+ * @param {any} fragment         //TODO: change Type to Fragment when class is implemented
+ * @returns {Promise<void>}
+ */
+function writeFragment(fragment) {
+  // Simulate db/network serialization of the value, storing only JSON representation.
+  // This is important because it's how things will work later with AWS data stores.
+  const serialized = JSON.stringify(fragment);
+  return metadata.put(fragment.ownerId, fragment.id, serialized);
+}
+
+/**
+ * Read a fragment's metadata from memory db
+ * @param {string} ownerId
+ * @param {string} id
+ * @returns {Promise<Object>}
+ */
+async function readFragment(ownerId, id) {
+  // NOTE: this data will be raw JSON, we need to turn it back into an Object.
+  // You'll need to take care of converting this back into a Fragment instance
+  // higher up in the callstack.
+  const serialized = await metadata.get(ownerId, id);
+  return typeof serialized === 'string' ? JSON.parse(serialized) : serialized;
+}
+
+/**
+ * Write a fragment's data buffer to memory db
+ * @param {string} ownerId
+ * @param {string} id
+ * @param {Buffer} buffer
+ * @returns {Promise<void>}
+ */
+function writeFragmentData(ownerId, id, buffer) {
+  return data.put(ownerId, id, buffer);
+}
+
+/**
+ * Write a fragment's data buffer to memory db. Returns a Promise
+ * @param {string} ownerId
+ * @param {string} id
+ * @returns {Promise<any>}
+ */
+function readFragmentData(ownerId, id) {
+  return data.get(ownerId, id);
+}
+
+/**
+ * Get a list of fragment IDs or full fragment objects for a given user from the memory db.
+ * @param {string} ownerId
+ * @param {boolean} [expand=false] - If true, return full fragment objects; if false, return only fragment IDs
+ * @returns {Promise<Array<Object>|Array<string>>} - A promise that resolves to an array of fragment IDs or objects
+ */
+async function listFragments(ownerId, expand = false) {
+  const fragments = await metadata.query(ownerId);
+  const parsedFragments = fragments.map((fragment) => JSON.parse(fragment));
+
+  // If we don't get anything back, or are supposed to give expanded fragments, return
+  if (expand || !fragments) {
+    return parsedFragments;
+  }
+
+  // Otherwise, map to only send back the ids
+  return parsedFragments.map((fragment) => fragment.id);
+}
+
+/**
+ * Delete a fragment's metadata and data from memory db
+ * @param {string} ownerId
+ * @param {string} id
+ * @returns {Promise<any>}
+ */
+function deleteFragment(ownerId, id) {
+  return Promise.all([
+    // Delete metadata
+    metadata.del(ownerId, id),
+    // Delete data
+    data.del(ownerId, id),
+  ]);
+}
+
+// INTERNAL USE ONLY - clear DBs. used for testing
+function _resetDBs() {
+  data.db = {};
+  metadata.db = {};
+}
+
+// Do not export function in production environment
+if (process.env.NODE_ENV !== 'production') {
+  module.exports._resetDBs = _resetDBs;
+}
+module.exports.listFragments = listFragments;
+module.exports.writeFragment = writeFragment;
+module.exports.readFragment = readFragment;
+module.exports.writeFragmentData = writeFragmentData;
+module.exports.readFragmentData = readFragmentData;
+module.exports.deleteFragment = deleteFragment;
---
+++ tests/unit/memory.test.js
@@ -0,0 +1,96 @@
+const { _resetDBs, readFragment, readFragmentData, writeFragment, writeFragmentData, listFragments, deleteFragment } = require('../../src/model/data/memory');
+
+
+describe('memory-db', () => {
+
+  // Clear DBs before each test
+  beforeEach(async () => {
+    _resetDBs()
+  });
+
+  test('writeFragment() returns nothing on successful write',  async () => {
+    const dataObj = { ownerId: 'a', id: 'b', value: 123 };
+    const result = await writeFragment(dataObj);
+    expect(result).toBe(undefined);
+  });
+
+  test('writeFragment() successfully overwrite existing fragment\'s metadata',  async () => {
+    await writeFragment({ ownerId: 'a', id: 'b', value: 123 });  // initial write
+    const result = await writeFragment({ ownerId: 'a', id: 'b', value: 300 });  // overwrite existing fragment
+    expect(result).toBe(undefined);
+    const value = await readFragment('a', 'b');
+    expect(value).toEqual({ ownerId: 'a', id: 'b', value: 300 });
+  });
+
+  test('readFragment() on non-existent primaryKey & secondaryKey pair returns nothing',  async () => {
+    const result = await readFragment('a', 'b');
+    expect(result).toBe(undefined);
+  });
+
+  test('readFragment() with incorrect secondaryKey returns nothing',  async () => {
+    writeFragment({ ownerId: 'a', id: 'b', value: 123 });
+    const result = await readFragment('a', 'c');
+    expect(result).toBe(undefined);
+  });
+
+  test('readFragment() returns metadata for what we writeFragment() into the db',  async () => {
+    const data = { ownerId: 'a', id: 'b', value: 123 };
+    writeFragment(data);
+    const result = await readFragment('a', 'b');
+    expect(result).toEqual(data);
+  });
+
+  test('writeFragmentData() returns nothing on successful write',  async () => {
+    const result = await writeFragmentData('a', 'b', 123);
+    expect(result).toBe(undefined);
+  });
+
+  test('writeFragmentData() successfully overwrites existing fragment\'s data buffer',  async () => {
+    await writeFragmentData('a', 'b', 123);  // initial write
+    const result = await writeFragmentData('a', 'b', 300);  // overwrite existing fragment
+    expect(result).toBe(undefined);
+    const value = await readFragmentData('a', 'b');
+    expect(value).toEqual(300);
+  });
+
+  test('readFragmentData() on non-existent primaryKey & secondaryKey pair returns nothing',  async () => {
+    const result = await readFragmentData('a', 'b');
+    expect(result).toBe(undefined);
+  });
+
+  test('readFragmentData() with incorrect secondaryKey returns nothing',  async () => {
+    await writeFragmentData('a', 'b', 123);
+    const result = await readFragmentData('a', 'c');
+    expect(result).toBe(undefined);
+  });
+
+  test('readFragmentData() returns fragment\'s data for what we writeFragmentData() into the db',  async () => {
+    await writeFragmentData('a', 'b', 123);
+    const result = await readFragmentData('a', 'b');
+    expect(result).toEqual(123);
+  });
+
+  test('writeFragment() expects string keys', () => {
+    const dataObj = { ownerId: 1, id: 1, value: 123 };
+    expect(async () => await writeFragment()).rejects.toThrow();
+    expect(async () => await writeFragment()(dataObj)).rejects.toThrow();
+  });
+
+   test('readFragment() expects string keys', () => {
+    expect(async () => await readFragment()).rejects.toThrow();
+    expect(async () => await readFragment(1)).rejects.toThrow();
+    expect(async () => await readFragment(1, 1)).rejects.toThrow();
+  });
+  
+  test('writeFragmentData() expects string keys', () => {
+    expect(async () => await writeFragmentData()).rejects.toThrow();
+    expect(async () => await writeFragmentData(1)).rejects.toThrow();
+    expect(async () => await writeFragmentData(1, 1, 123)).rejects.toThrow();
+  });
+
+  test('readFragmentData() expects string keys', () => {
+    expect(async () => await readFragmentData()).rejects.toThrow();
+    expect(async () => await readFragmentData(1)).rejects.toThrow();
+    expect(async () => await readFragmentData(1, 1)).rejects.toThrow();
+  }); 
+});
