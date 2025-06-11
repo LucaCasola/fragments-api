19eb70050214390f937183fa2cb16e0ff2297195
Luca Casola <lcasola@myseneca.ca>
2025-06-11 16:14:13 -0400

    Update to use createErrorResponse() & createSuccessResponse()

--- src/routes/api/get-id.js
+++ src/routes/api/get-id.js
@@ -2,22 +2,22 @@
 
 const { createSuccessResponse, createErrorResponse } = require('../../response');  // Used to create a success response object in HTTP responses
 const logger = require('../../logger');
-const { Fragment } = require('../../model/fragments');
+const { Fragment } = require('../../model/fragment');
 
 module.exports = async (req, res) => {
   const fragmentId = req.params.id;
   const ownerId = req.user;
 
   try {
-    logger.info(`Fetching fragment: ${fragmentId} for user: ${ownerId}`);
+    logger.info(`Fetching fragment: ${fragmentId}, for user: ${ownerId}`);
 
     const fragment = await Fragment.byId(ownerId, fragmentId);
     const data = await fragment.getData();
 
     res.setHeader('Content-Type', 'text/plain');
-    res.status(200).send(data);
+    res.status(200).json(createSuccessResponse(data));
   } catch (err) {
     logger.error(`Error fetching fragment ${fragmentId}: ${err.message}`);
-    res.status(404).json({ status: 'error', message: `Fragment not found: ${fragmentId}` });
+    res.status(404).json(createErrorResponse(404, `Fragment not found for fragmentId: ${fragmentId}`));
   }
 };
