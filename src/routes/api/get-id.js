588ec89a9b6bbd88da4b0cd09655acfd86c8fa89
Luca Casola <lcasola@myseneca.ca>
2025-06-12 17:55:26 -0400

    Return formatted fragmentData based on fragmentType

--- src/routes/api/get-id.js
+++ src/routes/api/get-id.js
@@ -12,12 +12,17 @@ module.exports = async (req, res) => {
     logger.info(`Fetching fragment: ${fragmentId}, for user: ${ownerId}`);
 
     const fragment = await Fragment.byId(ownerId, fragmentId);
-    const data = await fragment.getData();
+    const fragmentData = await fragment.getData();
 
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(200).json(createSuccessResponse(data));
+    if (fragment.type == 'text/plain') {
+      logger.info(`Fragment ${fragmentId} is of type text/plain, converting data to string`);
+      return res.status(200).json(createSuccessResponse({ fragmentData: fragmentData.toString() }));
+    } else {
+      logger.warn(`Fragment ${fragmentId} is of unknown type. Type is: ${fragment.type}`);
+      return res.status(415).json(createErrorResponse(415, `Unsupported fragment type: ${fragment.type} for fragmentId: ${fragmentId}`));
+    }
   } catch (err) {
     logger.error(`Error fetching fragment ${fragmentId}: ${err.message}`);
-    res.status(404).json(createErrorResponse(404, `Fragment not found for fragmentId: ${fragmentId}`));
+    return res.status(404).json(createErrorResponse(404, `Fragment not found for fragmentId: ${fragmentId}`));
   }
 };
