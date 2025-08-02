27e15d65c78827121afa28f57ef8006acdb409b5
Luca Casola <lcasola@myseneca.ca>
2025-08-02 15:10:37 -0400

    Update env structure to now have clearly defined AUTH_TYPE & DB_TYPE

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,27 +2,18 @@
 
 // Authorization configuration for the Fragments API. 
 // This module exports the appropriate authentication strategy based on environment variables.
-// It supports both AWS Cognito (production) and HTTP Basic Auth (development), but not both at the same time.
+// It supports both AWS Cognito (production) and HTTP Basic Auth (development)
 
-if (
-  process.env.AWS_COGNITO_POOL_ID &&
-  process.env.AWS_COGNITO_CLIENT_ID &&
-  process.env.HTPASSWD_FILE
-) {
-  throw new Error(
-    'env contains configuration for both AWS Cognito and HTTP Basic Auth. Only one is allowed.'
-  );
-}
 
-// Prefer Amazon Cognito (production)
-if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+// Amazon Cognito (production)
+if ((process.env.AUTH_TYPE == 'cognito') && process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
   module.exports = require('./cognito');
 }
 // Also allow for an .htpasswd file to be used (development)
-else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
+else if ((process.env.AUTH_TYPE == 'basic') && process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
   module.exports = require('./basic-auth');
 }
 // In all other cases, stop and fix our config
 else {
-  throw new Error('missing env vars: no authorization configuration found');
+  throw new Error('improper env vars: no proper authorization configuration found');
 }
--- src/model/data/index.js
+++ src/model/data/index.js
@@ -2,6 +2,18 @@
 
 // Back-end data configuration for the Fragments API. 
 // This module exports the appropriate memory strategy based on environment variables.
-// It supports both AWS backend data stores (TODO) (production) and in-memory database, but not both at the same time.
+// It supports both AWS backend data stores (production) and in-memory database (development), but not both at the same time.
 
-module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
+
+// AWS data stores (production)
+if ((process.env.DB_TYPE == 'aws') && process.env.AWS_REGION) {
+  module.exports = require('./aws');
+}
+// Also allow for memoryDB to be used (development)
+else if ((process.env.DB_TYPE == 'in-memory') && process.NODE_ENV !== 'production') {
+  module.exports = require('./memory');
+}
+// In all other cases, stop and fix our config
+else {
+  throw new Error('improper env vars: no proper database configuration found');
+}
